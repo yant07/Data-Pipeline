0122e8b647952f9270468e984c03b8d44f00637b
Tao Yan <tao.yan2@pfizer.com>
2022-11-15 15:19:51 +0800

    Update ch_cr_2d_list.py

--- data_pipeline/pipeline/ch_cr_2d_list.py
+++ data_pipeline/pipeline/ch_cr_2d_list.py
@@ -1,10 +1,10 @@
 import sys
-sys.path.insert(0, 'c:\\Users\\zhouy217\\OneDrive - Pfizer\\Documents\\data_pipeline\\app')
+sys.path.insert(0, 'c:\\Users\\Documents\\data_pipeline\\app')
  
 import text_mining_preprocess_0712
 
 def create_ch_2d_list():
-    ch_df=text_mining_preprocess_0712.read_file_as_dataframe("c:\\Users\\zhouy217\\OneDrive - Pfizer\\Documents\\data_pipeline\\data\\ch_test.xlsx",col_list=[0],name_list=['sentence'],file_header=None)
+    ch_df=text_mining_preprocess_0712.read_file_as_dataframe("c:\\Users\\Documents\\data_pipeline\\data\\ch_test.xlsx",col_list=[0],name_list=['sentence'],file_header=None)
     ch_text_mining=text_mining_preprocess_0712.TextMiningPreprocess(ch_df)
     ch_SentenceList=ch_text_mining.create_SentenceList()
     ch_WordList_in_SentenceList=ch_text_mining.chinese_text_cleaning(onlyChinese='yes')
