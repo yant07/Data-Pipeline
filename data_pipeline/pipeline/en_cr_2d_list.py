955b424c4a19f0439dea54e8672a9b8cdcdb22e3
Tao Yan <tao.yan2@pfizer.com>
2022-11-15 15:19:07 +0800

    Update en_cr_2d_list.py

--- data_pipeline/pipeline/en_cr_2d_list.py
+++ data_pipeline/pipeline/en_cr_2d_list.py
@@ -1,11 +1,11 @@
 import sys
-sys.path.insert(0, 'c:\\Users\\zhouy217\\OneDrive - Pfizer\\Documents\\data_pipeline\\app')
+sys.path.insert(0, 'c:\\Users\\Documents\\data_pipeline\\app')
  
 import utils as U
 import text_mining_preprocess_0712
 
 def create_en_2d_list():
-    en_df=text_mining_preprocess_0712.read_file_as_dataframe("c:\\Users\\zhouy217\\OneDrive - Pfizer\\Documents\\data_pipeline\\data\\en_test.xlsx",col_list=[0],name_list=['sentence'],file_header=None,IFna='no')
+    en_df=text_mining_preprocess_0712.read_file_as_dataframe("c:\\Users\\Documents\\data_pipeline\\data\\en_test.xlsx",col_list=[0],name_list=['sentence'],file_header=None,IFna='no')
     en_text_mining=text_mining_preprocess_0712.TextMiningPreprocess(en_df)
     en_SentenceList=en_text_mining.create_SentenceList()
     en_WordList_in_SentenceList=en_text_mining.english_text_cleaning(IFnum='yes',IFstem='no')
